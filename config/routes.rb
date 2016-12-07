94f84dfe8f165a905f0a2ae4dc911b1dfd299c6e
rkmemail1974 <rkm_email@yahoo.com>
2016-12-07 08:34:13 -0800

    feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :microposts, only: [:create, :destroy]
+  
   root to: 'static_pages#home'
 
 get '/signup', to: 'users#new'
